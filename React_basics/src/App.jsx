9882ff823d75119ecda1fd16893ec1fb08a20b2d
Tusharsing <tusharsingh0810@gmail.com>
2024-07-09 18:03:50 +0530

    Usestate

--- React_basics/src/App.jsx
+++ React_basics/src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 
 function App() {
-
+const[count,setcount] = useState(0);
   return (<>
-  
+   <button onClick={function(){
+    setcount(count + 1);
+   }}>count {count}</button>
   </>)
 }
 
